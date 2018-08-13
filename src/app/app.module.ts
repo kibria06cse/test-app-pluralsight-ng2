60778fcade614fc57ba9215747386cda52d29dfc
Abdus Salam Azad <kibria06cse@outlook.com>
2018-08-14 00:35:26 +0600

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,63 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { RouterModule, ActivatedRouteSnapshot } from '@angular/router'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
-import { EventsAppComponent } from './events-app.component';
-import { EventsListComponent } from './events/events-list.component'
-import { EventThumbnailComponent } from './events/event-thumbnail.component'
+import {
+  EventsListComponent,
+  EventThumbnailComponent,
+  EventService,
+  EventDetailsComponent,
+  CreateEventComponent,
+  EventRouteActivator,
+  EventListResolver,
+  CreateSessionComponent,
+  SessionListComponent
+} from './events/index'
+import { EventsAppComponent } from './events-app.component'
 import { NavBarComponent } from './nav/nav-bar.component'
-import { EventService } from './events/shared/event.service'
 import { ToastrService } from './common/toastr.service'
+import { appRoutes } from './routes'
+import { Error404Component } from './errors/404.component'
+import { AuthService } from './user/auth.service'
+import { CollapsibleWellComponent } from "./common/collapsible-well.component";
 
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   declarations: [
     EventsAppComponent,
     EventsListComponent,
     EventThumbnailComponent,
-    NavBarComponent
+    EventDetailsComponent,
+    NavBarComponent,
+    CreateEventComponent,
+    Error404Component,
+    CreateSessionComponent,
+    SessionListComponent,
+    CollapsibleWellComponent
   ],
-  providers: [EventService, ToastrService],
+  providers: [
+    EventService, 
+    ToastrService, 
+    EventRouteActivator,
+    EventListResolver,
+    AuthService,
+    { 
+      provide: 'canDeactivateCreateEvent', 
+      useValue: checkDirtyState 
+    }
+  ],
   bootstrap: [EventsAppComponent]
 })
-export class AppModule { }
+export class AppModule {}
+
+export function checkDirtyState(component:CreateEventComponent) {
+  if (component.isDirty)
+    return window.confirm('You have not saved this event, do you really want to cancel?')
+  return true
+}
--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit } from '@angular/core'
 import { EventService } from './shared/event.service'
 import { ToastrService } from '../common/toastr.service'
-
-declare let toastr
+import { ActivatedRoute } from '@angular/router'
+import { IEvent } from './shared/index'
 
 @Component({
-  selector: 'events-list',
   template: `
   <div>
     <h1>Upcoming Angular 2 Events</h1>
@@ -19,14 +18,14 @@ declare let toastr
   `
 })
 export class EventsListComponent implements OnInit {
-  events:any[]
+  events:IEvent[]
 
-  constructor(private eventService: EventService, private toastr: ToastrService) {
+  constructor(private eventService: EventService, private toastr: ToastrService, private route:ActivatedRoute) {
     
   }
 
   ngOnInit() {
-    this.events = this.eventService.getEvents()
+    this.events = this.route.snapshot.data['events']
   }
 
   handleThumbnailClick(eventName) {
